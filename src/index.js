2a9d49cb6514361372035ccc53698c1af718ad8f
Felipe Costa <felipecosta776@outlook.com>
2023-09-29 17:27:55 -0300

    firebase connection

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Update Message!");
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBGHGnHTIJw6GqFXePb9zuEXzQ3a6dTz4E",
+  authDomain: "fireflix-8af81.firebaseapp.com",
+  projectId: "fireflix-8af81",
+  storageBucket: "fireflix-8af81.appspot.com",
+  messagingSenderId: "417184782852",
+  appId: "1:417184782852:web:d4d5ba00703e7d499e3351"
+};
+
+const app = initializeApp(firebaseConfig);
